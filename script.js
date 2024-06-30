b3d6d67b16bd1fd2dd8de0a5147be584dda9a301
Nodirbek <khujamovcodes@gmail.com>
2024-06-30 23:23:27 +0500

    //isPalindrome

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function isPalindrome (text) {
-    const reverseText =text.split('').reverse().join('');
-    if (reverseText === text){
-        return true
-    } else {
-        return false
-    }
+//isPalindrome
+
+// function isPalindrome (text) {
+//     const reverseText =text.split('').reverse().join('');
+//     if (reverseText === text){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// let result = isPalindrome("arra");
+// console.log(result);
+
+function isPalindrome(text) {
+    const reverseText = text.split('').reverse().join('');
+    reverseText === text ? true : false
 }
 
 let result = isPalindrome("arra");
 console.log(result);
+
